66d51b81db2745fbc4e560b99f0b8ed053bb9b59
nikitabolkar123 <bolkarnikita2001@gmail.com>
2022-10-30 06:27:18 -0700

    added new file

---
+++ UseCase1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace UseCase1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random(); //it will create random number between 0 and 1
+            int value = random.Next(0, 2);
+             if(value == 0) 
+            { 
+                Console.WriteLine("Employee is present");
+        }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
